df2a98b8b9919179db720cad65e53d5ede388a6b
SJAR03 <sergio.ayerdis11825@est.uca.edu.ni>
2022-03-13 14:21:20 -0600

    Datos y entidades de inscripcion

---
+++ Security/src/main/java/entidades/Vw_inscripcion.java
@@ -0,0 +1,110 @@
+package entidades;
+
+public class Vw_inscripcion {
+
+	//Attributes
+	private int id_inscripcion;
+	private int id_usuario;
+	private String usario;
+	private int id_facultad;
+	private String nombre_facultad;
+	private int id_departamento;
+	private String nombre_departamento;
+	private int id_carrera;
+	private String nombre_carrera;
+	private String fecha_inscripcion;
+	private int id_oferta_detalle;
+	private String nombre_oferta;
+	private int id_escala;
+	private String calificacion;
+	
+	
+	//Methods
+	public int getId_inscripcion() {
+		return id_inscripcion;
+	}
+	public void setId_inscripcion(int id_inscripcion) {
+		this.id_inscripcion = id_inscripcion;
+	}
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getUsario() {
+		return usario;
+	}
+	public void setUsario(String usario) {
+		this.usario = usario;
+	}
+	public int getId_facultad() {
+		return id_facultad;
+	}
+	public void setId_facultad(int id_facultad) {
+		this.id_facultad = id_facultad;
+	}
+	public String getNombre_facultad() {
+		return nombre_facultad;
+	}
+	public void setNombre_facultad(String nombre_facultad) {
+		this.nombre_facultad = nombre_facultad;
+	}
+	public int getId_departamento() {
+		return id_departamento;
+	}
+	public void setId_departamento(int id_departamento) {
+		this.id_departamento = id_departamento;
+	}
+	public String getNombre_departamento() {
+		return nombre_departamento;
+	}
+	public void setNombre_departamento(String nombre_departamento) {
+		this.nombre_departamento = nombre_departamento;
+	}
+	public int getId_carrera() {
+		return id_carrera;
+	}
+	public void setId_carrera(int id_carrera) {
+		this.id_carrera = id_carrera;
+	}
+	public String getNombre_carrera() {
+		return nombre_carrera;
+	}
+	public void setNombre_carrera(String nombre_carrera) {
+		this.nombre_carrera = nombre_carrera;
+	}
+	public String getFecha_inscripcion() {
+		return fecha_inscripcion;
+	}
+	public void setFecha_inscripcion(String fecha_inscripcion) {
+		this.fecha_inscripcion = fecha_inscripcion;
+	}
+	public int getId_oferta_detalle() {
+		return id_oferta_detalle;
+	}
+	public void setId_oferta_detalle(int id_oferta_detalle) {
+		this.id_oferta_detalle = id_oferta_detalle;
+	}
+	public String getNombre_oferta() {
+		return nombre_oferta;
+	}
+	public void setNombre_oferta(String nombre_oferta) {
+		this.nombre_oferta = nombre_oferta;
+	}
+	public int getId_escala() {
+		return id_escala;
+	}
+	public void setId_escala(int id_escala) {
+		this.id_escala = id_escala;
+	}
+	public String getCalificacion() {
+		return calificacion;
+	}
+	public void setCalificacion(String calificacion) {
+		this.calificacion = calificacion;
+	}
+	
+	
+	
+}
