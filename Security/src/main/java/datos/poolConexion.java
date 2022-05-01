edad9f15c51b79e48f7253c21ce0e0ef9e89def1
kuon303 <alanespinales2013@gmail.com>
2022-05-01 08:59:16 -0600

    Pool de conexion corregido

--- Security/src/main/java/datos/poolConexion.java
+++ Security/src/main/java/datos/poolConexion.java
@@ -19,9 +19,9 @@ public class poolConexion
 	//private static String url = "jdbc:mysql://localhost:3306/dbfdocente?autoReconnect=true&useSSL=false&serverTimezone=UTC";
 	//private static String user = "root";
 	//private static String pass="2503";
-	private static String url = "jdbc:mysql://localhost:3306/gestion_docente?autoReconnect=true&useSSL=false&serverTimezone=America/Managua";
+	private static String url = "jdbc:mysql://165.98.12.158/gestion_docente?autoReconnect=true&useSSL=false&serverTimezone=America/Managua";
 	private static String user = "root";
-	private static String pass="abc123";
+	private static String pass="My$qlS3rv3rAPS*";
 	//prueba
 
     //Constructor
