0de4604d06384400c5bd1ac51d9f915a3c550a2d
SJAR03 <sayerdisrodriguez@gmail.com>
2022-05-02 02:32:34 -0500

    Opcion casi completo

--- Security/src/main/java/entidades/Tbl_opcion.java
+++ Security/src/main/java/entidades/Tbl_opcion.java
@@ -4,7 +4,8 @@ public class Tbl_opcion {
 	
 	//atributos
 	private int id_opcion;
-	private String opcion;
+	private String nombre_opcion;
+	private String descripcion;
 	private int estado;
 	
 	//metodos
@@ -14,11 +15,17 @@ public class Tbl_opcion {
 	public void setId_opcion(int id_opcion) {
 		this.id_opcion = id_opcion;
 	}
-	public String getOpcion() {
-		return opcion;
+	public String getNombre_opcion() {
+		return nombre_opcion;
 	}
-	public void setOpcion(String opcion) {
-		this.opcion = opcion;
+	public void setNombre_opcion(String nombre_opcion) {
+		this.nombre_opcion = nombre_opcion;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
 	}
 	public int getEstado() {
 		return estado;
@@ -26,6 +33,4 @@ public class Tbl_opcion {
 	public void setEstado(int estado) {
 		this.estado = estado;
 	}
-	
-	
 }
