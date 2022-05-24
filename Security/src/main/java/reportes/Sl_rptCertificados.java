003d3ac184361e216d01c367a904b457a9bab340
Gerald Diaz <gerald.diaz11715@est.uca.edu.ni>
2022-05-24 09:26:05 -0600

    reportes 2

--- Security/src/main/java/reportes/Sl_rptCertificados.java
+++ Security/src/main/java/reportes/Sl_rptCertificados.java
@@ -1,5 +1,25 @@
 package reportes;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datos.poolConexion;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.Exporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+
 public class Sl_rptCertificados {
 
 }
